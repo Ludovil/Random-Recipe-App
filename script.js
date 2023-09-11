8c5334e4b0e7b3409595c76a44e35e8857446e55
Ludovic Saule <ludovic_saule@hotmail.fr>
2023-09-11 10:55:44 +0200

    update html structure

--- script.js
+++ script.js
@@ -19,13 +19,46 @@ async function getRecipe() {
   const recipeTitle = document.createElement("h2");
   recipeTitle.innerText = recipe.strMeal;
   recipeSection.appendChild(recipeTitle);
-
   topSection.innerHTML = "";
+
   // Image
   const recipeImg = document.createElement("img");
   recipeImg.src = recipe.strMealThumb;
   topSection.appendChild(recipeImg);
   recipeSection.appendChild(topSection);
+
+  // Category header
+  const categoryHeader = document.createElement("h3");
+  categoryHeader.innerText = "Category";
+  categoryHeader.classList.add("category-header");
+  // Category
+  const recipeCategory = document.createElement("p");
+  recipeCategory.innerText = recipe.strCategory;
+  recipeCategory.classList.add("category");
+
+  topSection.appendChild(categoryHeader);
+  topSection.appendChild(recipeCategory);
+
+  // Area Header
+  const areaHeader = document.createElement("h3");
+  areaHeader.innerText = "Area";
+  areaHeader.classList.add("area-header");
+
+  const recipeArea = document.createElement("p");
+  recipeArea.innerText = recipe.strArea;
+
+  topSection.appendChild(areaHeader);
+  topSection.appendChild(recipeArea);
+
+  // Tags
+  const stringTags = recipe.strTags;
+  const arrayTags = stringTags.split(",");
+
+  arrayTags.forEach((element) => {
+    const recipeTag = document.createElement("p");
+    recipeTag.innerText = `#${element}`;
+    topSection.appendChild(recipeTag);
+  });
 
   // Ingredients & Measures
   const recipeIngredientsAndMeasures = document.createElement("ul");
